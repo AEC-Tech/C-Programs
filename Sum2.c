c8015a71931a767ff0604818e4b3a11e63aeb0f5
AEC-Tech <35284118+AEC-Tech@users.noreply.github.com>
2024-07-14 15:00:52 +0530

    Create Sum2.c

---
+++ Sum2.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    printf("Hello");
+    printf("\n--------------------------------");
+    printf("\n\nEnter two numbers to be added : ");
+    scanf("%d %d",&a,&b);
+    c = a + b;
+    printf("\n--------------------------------");
+    printf("\nSum of %d and %d is %d",a,b,c);
+    printf("\n--------------------------------");
+    
+    printf(" \n\nThank you");
+
+    return 0;
+}
